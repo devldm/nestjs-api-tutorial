84be4f59e6916d9dff5e1d594b346df4c9530257
liam <39243060+devldm@users.noreply.github.com>
2024-01-15 17:53:11 +0900

    user decorator, e2e testing, jwt guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
+import { JwtGuard } from '../auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
+
+  @Patch()
+  editUser() {}
 }
